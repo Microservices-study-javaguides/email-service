647f88f4a3f920516408c31591a4e42dc5d6a0e3
vit19988 <Mr.peerawit@live.com>
2024-02-19 00:37:26 +0700

    first commit

---
+++ src/test/java/org/whisky/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.whisky.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
